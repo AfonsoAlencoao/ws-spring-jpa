3699768735226ea2389ca1bd38b6c7449a3379c4
Afonso Marques <afonsoalencoaomarques@gmail.com>
2021-04-23 12:12:21 +0100

    Project created

---
+++ src/main/java/com/afonsomarques/wsspringjpa/WsSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.afonsomarques.wsspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afonsomarques/wsspringjpa/WsSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afonsomarques.wsspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
